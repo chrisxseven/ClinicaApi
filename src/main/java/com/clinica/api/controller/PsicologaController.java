4c168c8a71d65c6435a12b9396b82b6639d80702
Christian David <143462834+chrisxseven@users.noreply.github.com>
2025-01-24 09:23:51 -0300

    feat: adicionar método POST para salvar novos psicólogos

--- src/main/java/com/clinica/api/controller/PsicologaController.java
+++ src/main/java/com/clinica/api/controller/PsicologaController.java
@@ -5,10 +5,7 @@ import com.clinica.api.datasource.model.Psicologa;
 import com.clinica.api.repository.PsicologaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +25,10 @@ public class PsicologaController {
     public Optional<Psicologa> buscarPsicologaPorId(
             @PathVariable (name = "id", required = true) Long id) {
        return psicologaRepository.findById(id);
+    }
+
+    @PostMapping(path = "psicologa/save")
+    public void salvarPsicologa(@RequestBody Psicologa psicologa) {
+        psicologaRepository.save(psicologa);
     }
 }
