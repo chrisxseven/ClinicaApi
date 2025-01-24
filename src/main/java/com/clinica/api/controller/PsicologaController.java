f3bee8a433c3f1203905cd22bd997d4aa7697c5e
Christian David <143462834+chrisxseven@users.noreply.github.com>
2025-01-24 08:46:58 -0300

    feat: adicionar métodos GET para listar psicólogas e buscar por ID

--- src/main/java/com/clinica/api/controller/PsicologaController.java
+++ src/main/java/com/clinica/api/controller/PsicologaController.java
@@ -1,4 +1,32 @@
 package com.clinica.api.controller;
 
+
+import com.clinica.api.datasource.model.Psicologa;
+import com.clinica.api.repository.PsicologaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api")
 public class PsicologaController {
+
+    @Autowired
+    private PsicologaRepository psicologaRepository;
+
+    @GetMapping(path = "/psicologas")
+    public List<Psicologa> buscarPsicologa() {
+       return psicologaRepository.findAll();
+    }
+    @GetMapping(path = "/psicologa/id/{id}")
+    public Optional<Psicologa> buscarPsicologaPorId(
+            @PathVariable (name = "id", required = true) Long id) {
+       return psicologaRepository.findById(id);
+    }
 }
