803b70a0c3959f58126b8ea0fe2238c1c387da6a
chrisxseven <christiandvd2005@gmail.com>
2025-03-23 02:21:33 -0300

    feat: adicionar exceção personalizada para PacienteResource

--- src/main/java/com/clinica/api/exception/PsicologaResourceException.java
+++ src/main/java/com/clinica/api/exception/PsicologaResourceException.java
@@ -21,6 +21,6 @@ public class PsicologaResourceException extends Exception {
     }
 
     public PsicologaResourceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
