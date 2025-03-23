8dfa2d339684d0f9a1acb04df9875da00f0dd7cd
chrisxseven <christiandvd2005@gmail.com>
2025-03-23 01:30:36 -0300

    feat: adicionar serviço de cadastro de pacientes

---
+++ src/main/java/com/clinica/api/servicepaciente/CadastroPaciente.java
@@ -0,0 +1,33 @@
+package com.clinica.api.servicepaciente;
+
+import com.clinica.api.datasource.model.Paciente;
+import com.clinica.api.exception.PacienteResourceException;
+import com.clinica.api.repository.PacienteRepository;
+import com.clinica.api.resource.model.PacienteResource;
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroPaciente {
+
+    private static final Logger LOG = Logger
+            .getLogger(CadastroPaciente.class);
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private PacienteConversor service;
+
+    public void cadastro(PacienteResource pacienteResource) {
+
+        try{
+            Paciente paciente = service
+                    .conversor(pacienteResource);
+            pacienteRepository.saveAndFlush(paciente);
+        } catch (PacienteResourceException e) {
+            LOG.error("Erro ao salvar psicolog: " + e.getMessage(), e);
+        }
+    }
+}
